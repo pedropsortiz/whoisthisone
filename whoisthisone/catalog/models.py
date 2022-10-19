297efb6d1f5ef06234f01a4a84dd99d3209967c4
Pedro Ortiz <pedrops.ortiz@gmail.com>
2022-10-18 23:46:32 -0300

    Ajustei a função hidden para o primeiro e último nome da pessoa

--- whoisthisone/catalog/models.py
+++ whoisthisone/catalog/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 class Person:
     name_list = ['Alan Turing', 'Albert Einstein', 'Machado de Assis',
-            'Adolf Hitler', 'Cellbit', 'George Orwell',
+            'Adolf Hitler', 'George Orwell',
             'Giancarlo Esposito', 'Leonardo DiCaprio', 'Pedro Álvares Cabral',
             'Luís Inácio Lula da Silva', 'Jair Bolsonaro',
             'Robert Plant', 'Jack, o Estripador', 'Eminem',
+            'Gorillaz', 'Dante Alighieri', 'Leonardo da Vinci',
+            'Elvis Presley',
             ]
 
     def returnPerson(self):
